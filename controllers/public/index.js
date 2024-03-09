bc18eabfa278d3b5d5035c5375dd5a8277854705
xwkenneth <20220094@ricaldone.edu.sv>
2024-03-08 18:00:06 -0600

    Index.html
    Terminado

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -1,3 +1,10 @@
+// Constante para establecer el formulario de iniciar sesión.
+const SESSION_FORM = document.getElementById('sessionForm');
+MAIN.style.paddingTop = '0px';
+MAIN.style.paddingBottom = '0px';
+MAIN.style.paddingLeft = '0px';
+MAIN.classList.remove('container');
+ 
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async function() {
     await loadTemplate();
